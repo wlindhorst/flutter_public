c31ef48a1699b0b07aaeed9beeb3b8cb32d25891
William Lindhorst <lindhorst@sbcglobal.net>
2024-04-18 17:14:24 -0500

    Add animated favorite

--- meals/lib/screens/meal_details.dart
+++ meals/lib/screens/meal_details.dart
@@ -25,19 +25,30 @@ class MealDetailsScreen extends ConsumerWidget {
           title: Text(meal.title),
           actions: [
             IconButton(
-                onPressed: () {
-                  // onToggleFavorite(meal);
-                  final wasAdded = ref
-                      .read(favoriteMealsProvider.notifier)
-                      .toggleMealFavoriteStatus(meal);
-                  ScaffoldMessenger.of(context).clearSnackBars();
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    content: Text(wasAdded
-                        ? 'Meal added as a favorite.'
-                        : 'Meal removed.'),
-                  ));
-                },
-                icon: Icon(isFavorite ? Icons.star : Icons.star_border)),
+              onPressed: () {
+                // onToggleFavorite(meal);
+                final wasAdded = ref
+                    .read(favoriteMealsProvider.notifier)
+                    .toggleMealFavoriteStatus(meal);
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(
+                      wasAdded ? 'Meal added as a favorite.' : 'Meal removed.'),
+                ));
+              },
+              icon: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  transitionBuilder: (child, animation) {
+                    return RotationTransition(
+                      turns: Tween<double>(begin: 0.5, end: 1.0)
+                          .animate(animation),
+                      child: child,
+                    );
+                  },
+                  child: Icon(isFavorite ? Icons.star : Icons.star_border,
+                      key: ValueKey(
+                          isFavorite))), // use key which are the same time but have different data
+            ),
           ],
         ),
         body: SingleChildScrollView(
