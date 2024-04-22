cf314efd29af49dfbf3716c54adcdd3f11b7abd8
William Lindhorst <lindhorst@sbcglobal.net>
2024-04-22 14:12:07 -0500

    init save

---
+++ shopping_list/lib/widgets/new_item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() {
+    return _NewItemState();
+  }
+}
+
+class _NewItemState extends State<NewItem> {
+  
+}
