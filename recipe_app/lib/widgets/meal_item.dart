92742d66cda1eee73c07f0bfdba5c163ab748f5b
William Lindhorst <lindhorst@sbcglobal.net>
2025-09-23 15:30:54 -0500

    App update

--- recipe_app/lib/widgets/meal_item.dart
+++ recipe_app/lib/widgets/meal_item.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/models/meal.dart';
+import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
   final String title;
